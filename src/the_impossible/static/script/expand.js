42d08b5c8ed248d4ddd6546c66f922becb829bea
Michael <37400755+micha31r@users.noreply.github.com>
2020-04-10 13:35:53 +1200

    Added collapsible link animation

---
+++ src/the_impossible/static/script/expand.js
@@ -0,0 +1,26 @@
+function expand(element_id, button_id) {
+	var element = document.getElementById(element_id);
+	var button = document.getElementById("collapse-link-trigger");
+	if (button.name === "open") {
+		button.name = "close";
+		var style = `display: block !important;
+			position: absolute;
+			animation-name: popuplink;
+			animation-duration: 0.5s;
+			animation-fill-mode: forwards;
+			z-index: 1 !important;`;
+		element.style = style;
+	} else {
+		button.name = "open";
+		// Reset element css to default
+		element.style = "";
+	}
+}
+
+function reset_expand(element_id, button_id) {
+	var element = document.getElementById(element_id);
+	var button = document.getElementById(button_id);
+	button.name = "open";
+	// Reset element css to default
+	element.style = "";
+}
