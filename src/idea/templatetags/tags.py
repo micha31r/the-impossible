77aaa9070bf9dbd7fb74cc45da59f398805950e6
Michael <37400755+micha31r@users.noreply.github.com>
2020-04-22 15:03:00 +1200

    Added number truncate system

--- src/idea/templatetags/tags.py
+++ src/idea/templatetags/tags.py
@@ -1,7 +1,25 @@
 from django import template
+import math
 
 register = template.Library()
 
 @register.filter(name="times")
 def times(value):
 	return range(value)
+
+# Convert number to number to text e.g: 1100 -> 1.1k 
+@register.filter(name="ntt")
+def ntt(value):
+	string = ""
+	divided = math.floor(value / 1000)
+	if divided >= 1 and divided < 10:
+		string = f"{round(value/1000,1)}k"
+	elif divided >= 10 and divided < 1000:
+		string = f"{divided}k" 
+	elif divided >= 1000 and divided < 10000:
+		string = f"{round(divided/1000,1)}m" 
+	elif divided >= 10000:
+		string = "∞"
+	else:
+		string = value
+	return string
