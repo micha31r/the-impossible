c390395c4427bd934342e1a21f250b530d630518
Michael <37400755+micha31r@users.noreply.github.com>
2020-05-02 19:16:50 +1200

    Update resize.js

--- src/userupload/static/userupload/file_page/resize.js
+++ src/userupload/static/userupload/file_page/resize.js
@@ -2,6 +2,9 @@ function resize() {
 	$("#hidden").css("display", "block"); 
 	var parent_height = $("#hidden").height();
 	var parent_width = $("#hidden").width();
+	if (parent_height <= 5) {
+		parent_height = 350;
+	}
     $("#file-wrapper").css("height", `${parent_height}`); 
     $("#blur").css("height", `${parent_height}`); 
     $("#blur").css("width", `${parent_width}`); 
