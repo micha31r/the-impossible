68018eec3f530bd482d675428570360f8766630d
Michael <37400755+micha31r@users.noreply.github.com>
2020-05-04 10:06:50 +1200

    Update views.py

--- src/userupload/views.py
+++ src/userupload/views.py
@@ -37,34 +37,36 @@ def file_page(request, pk, referred_obj_name, referred_obj_pk, referred_obj_fiel
 					ctx['file_type'] = k
 					break
 
-		if form.is_valid() and request.FILES.get("file"):
-			if file_is_valid(request.FILES.get("file").name):	
+		if form.is_valid():
+			if request.FILES.get("file"):
+				if file_is_valid(request.FILES.get("file").name):	
+					data = form.cleaned_data
 
-				data = form.cleaned_data
-
-				# Create file
-				if not file:
-					file = File.objects.create(
-						user=get_object_or_404(Profile,user=request.user),
-						description = data.get("description"),
-						file = request.FILES.get("file")
-					)
-				else:
-					file.description = form.cleaned_data.get("description")
-					file.file = request.FILES.get("file")
+					# Create file
+					if not file:
+						file = File.objects.create(
+							user=get_object_or_404(Profile,user=request.user),
+							description = data.get("description"),
+							file = request.FILES.get("file")
+						)
+					else:
+						file.description = form.cleaned_data.get("description")
+						file.file = request.FILES.get("file")
+						file.save()
 					file.save()
-				file.save()
 
-				# Link file to parent object
-				globals()[referred_obj_field] = referred_obj_field
-				try: setattr(obj,globals()[referred_obj_field],file) # Set Foregin key field
-				except: 
-					try: getattr(obj,globals()[referred_obj_field]).add(file) # Add to many to many field 
-					except: pass
-				obj.save()
+					# Link file to parent object
+					globals()[referred_obj_field] = referred_obj_field
+					try: setattr(obj,globals()[referred_obj_field],file) # Set Foregin key field
+					except: 
+						try: getattr(obj,globals()[referred_obj_field]).add(file) # Add to many to many field 
+						except: pass
+					obj.save()
+					return redirect(f"{referred_obj_name.lower()}_edit_page",pk=referred_obj_pk)
+				else:
+					ctx["error"] = SERVER_ERROR["FILE"]
+			elif file.file:
 				return redirect(f"{referred_obj_name.lower()}_edit_page",pk=referred_obj_pk)
-			else:
-				ctx["error"] = SERVER_ERROR["FILE"]
 	else:
 		return redirect("access_error_page")
 	template_file = "userupload/file_page.html"
