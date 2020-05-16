b5772ef2e1a085e4c82a84724e82b20301a13358
Michael <37400755+micha31r@users.noreply.github.com>
2020-05-16 15:46:13 +1200

    Edited css for home page

--- src/the_impossible/static/home_page/resize.js
+++ src/the_impossible/static/home_page/resize.js
@@ -1,3 +1,4 @@
+// Resize notification
 function resize() {
 	var p_height = $("#navbar-notification").outerHeight();
 	$("#navbar-notification-close").height(p_height);
@@ -27,5 +28,44 @@ auto_run.queue(
 				$("#navbar-notification-close-svg").css("background-color","#000");
 			}
 		)
+	}
+);
+
+function navbar_css() {
+	if ($(window).width() < 770) {
+		var height = $("#navbar-notification").outerHeight() + $(".navbar").outerHeight() + 50;
+		$(".earth").css("top",`${height}px`);
+		$(".intro").css("margin-bottom",`${$(".card:eq(1)").height() + 50}px`);
+	} else {
+		$(".earth").removeAttr("style");
+		$(".intro").removeAttr("style");
+	}
+	
+	// Set and reset navbar css
+	if ($(window).width() > 575) {
+		$(".navbar").css(
+			{
+	    		"border": "0",
+				"background-color": "rgba(0,0,0,0) !important",
+				"position": "relative !important",
+				"transform": "translateY(100%)",
+				"margin-top": "-100px",
+			}
+		);
+		/* 	Intro's margin top need to account for navbar's height because 
+			in desktop resolution navbar is positioned absolute. */
+		$(".intro").css("margin-top",`${$(".navbar").outerHeight() + 40}px`);
+	} else {
+		$(".navbar").removeAttr("style");
+	}
+}
+
+auto_run.queue(
+	function() {
+		navbar_css();
+		window.addEventListener(
+			"resize", 
+			navbar_css
+		);
 	}
 );
