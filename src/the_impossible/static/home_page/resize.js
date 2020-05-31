bbda86a77156fabed6deedca0f6c24423d51bf3d
Michael <37400755+micha31r@users.noreply.github.com>
2020-05-31 17:58:33 +1200

    Edited things

--- src/the_impossible/static/home_page/resize.js
+++ src/the_impossible/static/home_page/resize.js
@@ -1,7 +1,10 @@
-// Resize notification
 function resize() {
-	var p_height = $("#navbar-notification").outerHeight();
-	$("#navbar-notification-close").height(p_height);
+	var height = 600 - $("nav").outerHeight() - 100;
+	if ($(window).height() > 600) {
+		height = $(window).height() - $("nav").outerHeight() - 100;
+	}
+	$(".wrapper").first().height(`${height}px`);
+	$(".black").height($("body").height());
 }
 
 auto_run.queue(
@@ -9,66 +12,7 @@ auto_run.queue(
 		resize();
 		window.addEventListener(
 			"resize", 
-			resize
-		);
-	}
-);
-
-auto_run.queue(
-	function() {
-		$("#navbar-notification-close").hover(
-			// Hover
-			function() {
-				$("#navbar-notification-close-svg").css("stroke","#000");
-				$("#navbar-notification-close-svg").css("background-color","#FFF");
-			},
-			// Un-hover
-			function() {
-				$("#navbar-notification-close-svg").css("stroke","#FFF");
-				$("#navbar-notification-close-svg").css("background-color","#000");
-			}
-		)
-	}
-);
-
-
-// Things todo with navbar
-function navbar_css() {
-	if ($(window).width() < 770) {
-		var height = $("#navbar-notification").outerHeight() + $(".navbar").outerHeight() + 50;
-		$(".earth").css("top",`${height}px`);
-		$(".intro").css("margin-bottom",`${$(".card:eq(1)").height() + 50}px`);
-	} else {
-		$(".earth").removeAttr("style");
-		$(".intro").css("margin-bottom","");
-	}
-
-	// Set and reset navbar css
-	if ($(window).width() > 575) {
-		$(".navbar").css(
-			{
-	    		"border": "0",
-				"background-color": "rgba(0,0,0,0) !important",
-				"position": "relative !important",
-				"transform": "translateY(100%)",
-				"margin-top": "-100px",
-			}
-		);
-		/* 	Intro's margin top need to account for navbar's height because 
-			in desktop resolution navbar is positioned absolute. */
-		$(".intro").css("margin-top",`${$(".navbar").outerHeight() + 40}px`);
-	} else {
-		$(".navbar").removeAttr("style");
-		$(".intro").css("margin-top","40px");
-	}
-}
-
-auto_run.queue(
-	function() {
-		navbar_css();
-		window.addEventListener(
-			"resize", 
-			navbar_css
+			function(){ resize(); }
 		);
 	}
 );
