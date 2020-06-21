458ea3825f6b0d80411204712a6cb5cd2db4b46a
Michael <37400755+micha31r@users.noreply.github.com>
2020-06-21 14:01:00 +1200

    Edited things

--- src/userupload/urls.py
+++ src/userupload/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 
 from .views import (
-	idea_header_img_page,
-	idea_body_img_page,
+	idea_img_page,
 	delete_page
 )
 
 urlpatterns = [
-	path('file/idea/header/<int:idea_pk>/<slug:file_pk>/', idea_header_img_page, name="userupload_idea_header_img_page"),
-	path('file/idea/body/<int:idea_pk>/<slug:file_pk>/', idea_body_img_page, name="userupload_idea_body_img_page"),
+	path('file/idea/header/<int:idea_pk>/<slug:file_pk>/<str:field>', idea_img_page, name="userupload_idea_img_page"),
 	path('file/<slug:redirect_name>/<int:obj_pk>/<slug:file_pk>/', delete_page, name="userupload_delete_page"),
 ] 
