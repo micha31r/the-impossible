f68eae2bd2db0c718e9211eacaaeea0fc899103e
Michael <37400755+micha31r@users.noreply.github.com>
2020-06-24 11:47:12 +1200

    Edited notification resize function
    Made it more responsive

--- src/usermgmt/static/usermgmt/account_dashboard_page/resize.js
+++ src/usermgmt/static/usermgmt/account_dashboard_page/resize.js
@@ -1,7 +1,15 @@
 function notification_resize() {
+	// Width 
+	var container_width = $(".user-info").innerWidth() - 80;
+	var user_info_container = $(".user-info-container").outerWidth();
+	var padding = 40;
+	var new_width = container_width - user_info_container - padding - 20; // 20px spacing
+	$(".notification-container").width(new_width);
+	// Height
 	var padding_top = parseInt($(".notification-container").css("padding-top").replace("px",""));
 	var new_height = $(".user-info").height() - padding_top;
 	$(".notification-container").height(new_height);
+
 }
 
 auto_run.queue( 
