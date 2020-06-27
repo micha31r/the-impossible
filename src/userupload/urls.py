cb07f02e7e8df79697de35a966b8f08194e74818
Michael <37400755+micha31r@users.noreply.github.com>
2020-06-27 19:57:12 +1200

    Edited things

--- src/userupload/urls.py
+++ src/userupload/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 
 from .views import (
 	idea_img_page,
-	delete_page
+	idea_img_delete_page,
+	profile_img_page,
+	profile_img_delete_page,
 )
 
 urlpatterns = [
 	path('file/idea/header/<int:idea_pk>/<slug:file_pk>/<str:field>', idea_img_page, name="userupload_idea_img_page"),
-	path('file/<slug:redirect_name>/<int:obj_pk>/<slug:file_pk>/', delete_page, name="userupload_delete_page"),
+	path('file/delete/idea/<slug:redirect_name>/<int:obj_pk>/<slug:file_pk>/', idea_img_delete_page, name="userupload_idea_img_delete_page"),
+
+	path('file/profile/header/<int:profile_pk>/<slug:file_pk>/', profile_img_page, name="userupload_profile_img_page"),
+	path('file/delete/profile/<slug:redirect_name>/<slug:username>/<str:content_filter>/<int:page_num>/<slug:file_pk>/', profile_img_delete_page, name="userupload_profile_img_delete_page"),
 ] 
--- src/userupload/views.py
+++ src/userupload/views.py
@@ -8,6 +8,8 @@ from .models import *
 
 from idea.models import Idea
 
+from usermgmt.models import Profile
+
 from the_impossible.utils import *
 
 from the_impossible.ERROR import *
@@ -139,12 +141,71 @@ def idea_img_page(request,idea_pk,file_pk,field):
 				return redirect(redirect_name,pk=idea_pk)
 	else:
 		return redirect("access_error_page")
-	template_file = "userupload/img_page.html"
+	template_file = "userupload/idea_img.html"
 	return render(request,template_file,ctx)
 
 @login_required
-def delete_page(request,redirect_name,obj_pk,file_pk):
+def idea_img_delete_page(request,redirect_name,obj_pk,file_pk):
 	file = get_object_or_404(File, pk=file_pk)
 	file.delete()
 	return redirect(redirect_name,pk=obj_pk)
+
+@login_required
+def profile_img_page(request,profile_pk,file_pk):
+	ctx = {} # Context variables
+	ctx["date"] = Date()
+	ctx["file"] = file = File.objects.filter(pk=file_pk.isdigit() and file_pk or -1).first()
+	ctx["form"] = form = FileForm(request.POST or None, request.FILES or None)
+	ctx["redirect_name"] = redirect_name = "account_dashboard_page"
+
+	ctx["obj"] = profile = get_object_or_404(Profile, pk=profile_pk)
+
+	if profile.user == request.user:
+		# Set default values
+		if file:
+			form.fields["description"].initial = file.description
+			form.fields["file"].initial = file.file
+
+		if form.is_valid():
+			uploaded_file = request.FILES.get("file")
+			if uploaded_file:
+				if file_is_valid(uploaded_file.name,"image"):	
+					data = form.cleaned_data
+
+					# Create file
+					if not file:
+						file = File.objects.create(
+							user=request.user,
+							description = data.get("description"),
+							file = uploaded_file
+						)
+					else: # replace file
+						file.description = form.cleaned_data.get("description")
+						file.file = uploaded_file
+						file.save()
+					file.save()
+
+					# Link file to parent object
+					profile.profile_img = file
+					profile.save()
+					return redirect(redirect_name,username=request.user.username,content_filter="my",page_num=1)
+
+				# Invalid file type
+				else:
+					ctx["error"] = SERVER_ERROR["FILE"]
+			# If user didn't replace the old file
+			elif file.file:
+				# This only work if the url name starts with the model's name. E.G: idea_edit_page
+				return redirect(redirect_name,username=request.user.username,content_filter="my",page_num=1)
+	else:
+		return redirect("access_error_page")
+	template_file = "userupload/profile_img.html"
+	return render(request,template_file,ctx)
 	
+@login_required
+def profile_img_delete_page(request,redirect_name,username,content_filter,page_num,file_pk):
+	file = get_object_or_404(File, pk=file_pk)
+	file.delete()
+	return redirect(redirect_name,username=username,content_filter=content_filter,page_num=page_num)
+
+
