df734d6610e789621f530de019e75168f49aedf2
Michael <37400755+micha31r@users.noreply.github.com>
2020-06-28 20:40:08 +1200

    Edited notification stuff

--- src/usermgmt/views.py
+++ src/usermgmt/views.py
@@ -117,9 +117,12 @@ def account_dashboard_page(request,username,content_filter,page_num):
 	ctx["encrypted_string"] = encrypt(request.user.username)
 	# The profile for the viewed user
 	ctx["target_profile"] = target_profile = get_object_or_404(Profile,user=user)
-	# Get 20 most recent notifications
-	ctx["target_notifications"] = target_profile.notification.all().order_by('-timestamp')[:20]
-	
+	# Get 20 most recent notifications and dismiss them
+	ctx["target_notifications"] = target_notifications = target_profile.notification.all().order_by('-timestamp')[:20]
+	for notification in target_notifications:
+		notification.message_status = 3
+		notification.save()
+
 	# Followers
 	ctx["followers"] = followers = User.objects.filter(profile__following=target_profile.user)
 	ctx["followers_profile"] = []
