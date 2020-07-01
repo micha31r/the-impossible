b626622a2e948b84dc21145a53e9c5b2074a3ee5
Michael <37400755+micha31r@users.noreply.github.com>
2020-07-01 21:45:55 +1200

    Edited things

---
+++ src/idea/static/idea/detail_page/resize.js
@@ -0,0 +1,35 @@
+function image_resize() {
+	if ($(window).width() < 750) {
+		$(".right-container").children().each(
+			function() {
+				$(this).appendTo("#append-target1");
+			}
+		);
+		$(".right-container").css("display","none");
+		$(".left-container").css("width","100%");
+	} else {
+		$("#append-target1").children().each(
+			function() {
+				$(this).appendTo(".right-container");
+			}
+		);
+		$(".left-container").css("width","");
+		$(".right-container").css("display","");
+
+		var new_width = $(".wrapper").width() - $(".left-container").outerWidth() - 30;
+		console.log($(".wrapper").width());
+		console.log($(".left-container").outerWidth());
+		console.log(new_width);
+		$(".right-container").width(new_width);
+	}
+}
+
+auto_run.queue( 
+	function() {
+		image_resize();
+		window.addEventListener(
+			"resize", 
+			image_resize
+		);
+	}
+);
