76f52e921a80767e19e3447b50b4c379d252de16
Michael <37400755+micha31r@users.noreply.github.com>
2020-07-12 16:02:07 +1200

    Edited things

--- src/usermgmt/static/usermgmt/account_meet_page/resize.js
+++ src/usermgmt/static/usermgmt/account_meet_page/resize.js
@@ -1,26 +1,30 @@
-function profile_resize() {
-	// Width 
+function layout_resize() {
 	var container_width = $(".search-user-content").innerWidth();
-	var sibling_width = $(".recent-idea-container").outerWidth();
-	// Minus extra 10px just in case
-	$(".search-profile-container").width(container_width-sibling_width-30);
-
 	if ($(window).width() < 1020) {
 		$(".search-user-content").prepend($(".search-profile-container"));
-		$(".search-profile-container").width(container_width - 42); // 2 is border width (L & R)
+		$(".search-profile-container").width(container_width); // 2 is border width (L & R)
 		$(".recent-idea-container").width(container_width);
+	} else {
+		$(".search-user-content").append($(".search-profile-container")); 
+		$(".recent-idea-container").width(); 
+		var sibling_width = $(".recent-idea-container").outerWidth();
+		// Minus extra 10px just in case
+		$(".search-profile-container").width(container_width-sibling_width-30);
 	}
-	
+
+	if ($(window).width() < 720) {
+		$(".wrapper").prepend($(".right"));
+	} else {
+		$(".wrapper").append($(".right"));
+	}
 }
 
 auto_run.queue( 
 	function() {
-		profile_resize();
+		layout_resize();
 		window.addEventListener(
 			"resize", 
-			function() {
-				profile_resize();
-			}
+			layout_resize
 		);
 	}
 );
