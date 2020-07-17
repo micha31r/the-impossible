621ccde718f4b09c7eb72555d0a6a30106ced8d9
Michael <37400755+micha31r@users.noreply.github.com>
2020-07-17 13:43:16 +1200

    Edited things

--- src/the_impossible/urls.py
+++ src/the_impossible/urls.py
@@ -37,5 +37,5 @@ urlpatterns = [
     path('support/', include("support.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
