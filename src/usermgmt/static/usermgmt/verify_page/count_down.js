689114f284c4b7955510512bdfa60a90f4a8cf27
Michael <37400755+micha31r@users.noreply.github.com>
2020-07-25 11:31:07 +1200

    Update count_down.js

--- src/usermgmt/static/usermgmt/verify_page/count_down.js
+++ src/usermgmt/static/usermgmt/verify_page/count_down.js
@@ -2,16 +2,19 @@ var process_id;
 var timer = 60;
 
 function start_timer() {
-	process_id = setInterval(
-		function() {
-			timer -= 1;
-			document.getElementById("resend-link-text").innerHTML = `Resend in ${timer}s`;
-			if (timer <= 0) { 
-				window.location.href = resend_link;
-				timer = 60;
-				clearTimeout(process_id);
-			}
-		}, 
-		1000
-	);
+	if (!process_id && timer == 60) {
+		process_id = setInterval(
+			function() {
+				timer -= 1;
+				document.getElementById("resend-link-text").innerHTML = `Resend in ${timer}s`;
+				if (timer <= 0) { 
+					window.location.href = resend_link;
+					timer = 60;
+					process_id = null;
+					clearTimeout(process_id);
+				}
+			}, 
+			1000
+		);
+	}
 }
