f6a2caf364a4a4763a690b7d5ff9b6f11d5a9e8b
Michael <37400755+micha31r@users.noreply.github.com>
2020-08-01 17:59:26 +1200

    Added url namespace to userupload app

--- src/userupload/urls.py
+++ src/userupload/urls.py
@@ -7,10 +7,12 @@ from .views import (
 	profile_img_delete_page,
 )
 
-urlpatterns = [
-	path('file/idea/<int:idea_pk>/<slug:file_pk>/<str:field>', idea_img_page, name="userupload_idea_img_page"),
-	path('file/delete/idea/<slug:redirect_name>/<int:obj_pk>/<slug:file_pk>/', idea_img_delete_page, name="userupload_idea_img_delete_page"),
+app_name = 'userupload'
 
-	path('file/profile/<int:profile_pk>/<slug:file_pk>/', profile_img_page, name="userupload_profile_img_page"),
-	path('file/delete/profile/<slug:redirect_name>/<slug:username>/<str:content_filter>/<int:page_num>/<slug:file_pk>/', profile_img_delete_page, name="userupload_profile_img_delete_page"),
+urlpatterns = [
+	path('file/idea/<int:idea_pk>/<slug:file_pk>/<str:field>', idea_img_page, name="idea_img_page"),
+	path('file/delete/idea/<slug:redirect_name>/<int:obj_pk>/<slug:file_pk>/', idea_img_delete_page, name="idea_img_delete_page"),
+
+	path('file/profile/<int:profile_pk>/<slug:file_pk>/', profile_img_page, name="profile_img_page"),
+	path('file/delete/profile/<slug:redirect_name>/<slug:username>/<str:content_filter>/<int:page_num>/<slug:file_pk>/', profile_img_delete_page, name="profile_img_delete_page"),
 ] 
