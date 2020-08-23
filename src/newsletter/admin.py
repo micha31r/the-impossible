0a5d3380e7440f5ceafbe65bc0cfe4293dc2eb73
Michael <37400755+micha31r@users.noreply.github.com>
2020-08-23 12:58:09 +1200

    Update admin.py

--- src/newsletter/admin.py
+++ src/newsletter/admin.py
@@ -4,8 +4,8 @@ from .models import Subscriber
 
 class SubscriberAdmin(admin.ModelAdmin):
     # Display custom fields in Django admin
-    list_display = ('email',"last_sent","id","timestamp")
-    search_fields = ('email',)
+    list_display = ("email","slug","last_sent","id","timestamp")
+    search_fields = ("email",)
     readonly_fields = ["timestamp"]
 
 admin.site.register(Subscriber,SubscriberAdmin)
